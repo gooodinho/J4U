6d3783231bf4d33994598321ba263a1b574064fe
gooodinho <andrey09132000@gmail.com>
2021-04-13 13:39:19 +0300

    new start keyboard

---
+++ keyboards/default/default_keyboards.py
@@ -0,0 +1,21 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from utils import format_text
+
+
+def get_start_keyboard(lang: str = 'ru'):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+            [
+                KeyboardButton(text=format_text('btn finding job', lang)),
+                KeyboardButton(text=format_text('btn bookmarks', lang))
+            ],
+            [
+                KeyboardButton(text=format_text('btn cv', lang)),
+                KeyboardButton(text=format_text('btn mailing', lang))
+            ],
+            [
+                KeyboardButton(text=format_text('btn settings', lang)),
+                KeyboardButton(text=format_text('btn feedback', lang))
+            ]
+        ]
+    )
+    return keyboard
